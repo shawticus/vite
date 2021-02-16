5ba8ebd0d7087314079364a69b907cbefca5bfd0
Harry Brundage <harry.brundage@gmail.com>
2021-02-16 13:48:56 -0500

    Add example of broken dep scanning for SSR with virtual modules

--- packages/playground/ssr-react/vite.config.js
+++ packages/playground/ssr-react/vite.config.js
@@ -1,17 +1,29 @@
+const virtual = require('rollup-plugin-virtual')
+
 const reactRefresh = require('@vitejs/plugin-react-refresh')
 
 /**
  * @type {import('vite').UserConfig}
  */
 module.exports = {
-  plugins: [reactRefresh()],
+  plugins: [
+    reactRefresh(),
+    virtual({
+      'my-cool-module.tsx': `import React from 'react';
+      export default 1`
+    })
+  ],
   esbuild: {
     jsxInject: `import React from 'react';`
   },
   build: {
     minify: false,
     rollupOptions: {
-      input: ['./src/entry-about.jsx', './src/entry-home.jsx']
+      input: {
+        about: './src/entry-about.jsx',
+        home: './src/entry-home.jsx',
+        virtual: 'my-cool-module.tsx'
+      }
     }
   }
 }
